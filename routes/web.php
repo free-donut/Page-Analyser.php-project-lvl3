482ce9d23e945bf808c6d1d445f2d033d2e9c22f
free-donut <yula@freedonut.space>
2019-10-29 23:14:04 +0300

    add routers navbar

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,20 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+$router->get('foo', function () {
+    return 'Hello World';
+});
+$router->post('foo', function () {
+    //
+});
+$router->get('user/{id}', 'UserController@show');
+$router->get('home', function () {
+    return response($content, $status)
+                  ->header('Content-Type', $value);
+});
+Route::get('navbar', function () {
+    return view('navbar', ['name' => 'James']);
 });
