2816065f59bdf8dce6e10d625c8cf8b957c3b2fa
free-donut <yula@freedonut.space>
2020-01-24 13:29:36 +0300

    modified migrations

--- database/migrations/2019_11_04_152617_create_domains_table.php
+++ database/migrations/2019_11_04_152617_create_domains_table.php
@@ -24,8 +24,6 @@ class CreateDomainsTable extends Migration
 
     public function down()
     {
-    	//Schema::rename('Domains', 'domains');
-    	Schema::dropIfExists('Domains');
         Schema::dropIfExists('domains');
     }
 }
